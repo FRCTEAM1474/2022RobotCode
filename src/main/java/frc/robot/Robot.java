7a109a68997431226c2719b8811be2c43f933aac
FRCTEAM1474 <frcteam1474@gmail.com>
2022-01-26 20:57:51 -0500

    Initial support for the red and blue light patterns for LED strips. Still needs button support.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,6 +4,8 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.AddressableLED;
+import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
@@ -12,16 +14,18 @@ import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
-/**
- * This is a demo program showing the use of the DifferentialDrive class. Runs the motors with
- * arcade steering.
- */
+
 public class Robot extends TimedRobot {
   // maps the MOTORS to the MOTOR CONTROLLERS
   private final PWMSparkMax m_motorZero = new PWMSparkMax(0);
   private final PWMSparkMax m_motorOne = new PWMSparkMax(1);
   private final PWMSparkMax m_motorTwo = new PWMSparkMax(2);
   private final PWMSparkMax m_motorThree = new PWMSparkMax(3);
+
+  public AddressableLED m_led;
+  public AddressableLEDBuffer m_ledBuffer;
+  public int m_redflux_FirstPixelHue;
+  public int m_blueflux_FirstPixelHue;
 
   MotorControllerGroup m_Right = new MotorControllerGroup(m_motorZero, m_motorOne);
   MotorControllerGroup m_Left = new MotorControllerGroup(m_motorTwo, m_motorThree);
@@ -47,7 +51,6 @@ public class Robot extends TimedRobot {
 
     flywheel = new FlywheelSubsystem();
 
-
     JoystickButton intakeInButton = new JoystickButton(m_stick, 2);
 
     JoystickButton outtakeButton = new JoystickButton(m_stick, 5);
@@ -57,7 +60,6 @@ public class Robot extends TimedRobot {
     JoystickButton secondaryOuttakeButton = new JoystickButton(m_stick, 6);
 
     JoystickButton flywheelButton = new JoystickButton(m_stick, 1);
-
 
     intakeInButton.whileActiveOnce(new IntakeCommand(.5));
 
@@ -69,6 +71,15 @@ public class Robot extends TimedRobot {
 
     flywheelButton.whileActiveOnce(new FlywheelCommand(1));
     
+    m_led = new AddressableLED(5);
+  
+    m_ledBuffer = new AddressableLEDBuffer(60);
+
+    m_led.setLength(m_ledBuffer.getLength());
+    
+    m_led.setData(m_ledBuffer);
+
+    m_led.start();
   }
 
   @Override
@@ -77,10 +88,42 @@ public class Robot extends TimedRobot {
     // That means that the Y axis drives forward
     // and backward, and the X turns left and right.
     m_robotDrive.arcadeDrive(m_stick.getY(), -m_stick.getX());
+    
   }
 
   @Override
   public void robotPeriodic() {
     CommandScheduler.getInstance().run();
+    
+    m_led.setData(m_ledBuffer);
+    
+    
+  }
+
+  public void blueflux(){
+    for (var i = 0; i < m_ledBuffer.getLength(); i++) {
+      final var blue = (m_blueflux_FirstPixelHue + (i * 180 / m_ledBuffer.getLength())) % 180;
+      //m_ledBuffer.setHSV(i, 196, sat, 84);
+      m_ledBuffer.setRGB(i, 0, 0, blue);
+    }
+    m_blueflux_FirstPixelHue += 1;
+
+    m_blueflux_FirstPixelHue %= 180;
+    //for (var i = 0; i < m_ledBuffer.getLength(); i++) {
+      //m_ledBuffer.setRGB(i, 0, 0, 255);
+    //}
+  }
+  public void redflux(){
+    for (var i = 0; i < m_ledBuffer.getLength(); i++) {
+      final var red = (m_redflux_FirstPixelHue + (i * 180 / m_ledBuffer.getLength())) % 180;
+      //m_ledBuffer.setHSV(i, 196, sat, 84);
+      m_ledBuffer.setRGB(i, red, 0, 0);
+    }
+    m_redflux_FirstPixelHue += 1;
+
+    m_redflux_FirstPixelHue %= 180;
+    //for (var i = 0; i < m_ledBuffer.getLength(); i++) {
+      //m_ledBuffer.setRGB(i, 0, 0, 255);
+    //}
   }
 }
